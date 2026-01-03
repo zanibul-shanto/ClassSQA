66bd27720e5163191305acf520ec8d12bfbb7f5b
Shanto <zanibul.shanto@gmail.com>
2026-01-03 19:37:48 +0600

    3rd fix

--- functions.py
+++ functions.py
@@ -8,4 +8,6 @@ def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
+    # return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
+    assert fahrenheit >= -459.67, "Temperature below absolute zero!"
+    return multiply(subtract(fahrenheit, 32), 5 / 9)
