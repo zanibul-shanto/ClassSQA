d20891af8f67e0c9a03ab7926443a18db144eb21
Shanto <zanibul.shanto@gmail.com>
2026-01-03 19:38:00 +0600

    3rd fix minor

--- test_functions.py
+++ test_functions.py
@@ -11,8 +11,8 @@ def test_subtract():
    assert subtract(2, 3) == -1
 
 # uncomment the following test in step 11
-# def test_convert_fahrenheit_to_celsius():
-#    assert f2c(32) == 0
-#    assert f2c(122) == pytest.approx(50)
-#    with pytest.raises(AssertionError):
-#        f2c(-600)
+def test_convert_fahrenheit_to_celsius():
+   assert f2c(32) == 0
+   assert f2c(122) == pytest.approx(50)
+   with pytest.raises(AssertionError):
+       f2c(-600)
